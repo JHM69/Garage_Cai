7acf3e3ac7ed82ca79c384a562c8c29dbb6cb6f2
jhm69 <jahangirhmarup@gmail.com>
2023-01-27 04:55:48 +0600

    authentication added

---
+++ app/src/main/java/com/bitsnbites/garagecai/login/MiddleActivity.java
@@ -0,0 +1,19 @@
+package com.bitsnbites.garagecai.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bitsnbites.garagecai.R;
+import com.bitsnbites.garagecai.model.User;
+
+public class RegistrationActivity extends AppCompatActivity {
+    User user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        user.
+    }
+}
